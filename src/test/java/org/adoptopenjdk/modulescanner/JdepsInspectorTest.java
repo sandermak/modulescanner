57cf2bd36523b222d57570392ae0c841b6dbc2f3
Christian Stein <sormuras@gmail.com>
2018-07-04 21:52:41 +0200

    Upgrade to JUnit 5
    That is JUnit Jupiter 5.2 with some cleanup in the test files.
    Also excluding blank lines from the JdepsInspector violations.

--- src/test/java/org/adoptopenjdk/modulescanner/JdepsInspectorTest.java
+++ src/test/java/org/adoptopenjdk/modulescanner/JdepsInspectorTest.java
@@ -1,15 +1,15 @@
 package org.adoptopenjdk.modulescanner;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.nio.file.Paths;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
-
-public class JdepsInspectorTest {
+class JdepsInspectorTest {
 
     @Test
-    public void testNonViolatingJar() {
+    void testNonViolatingJar() {
         JdepsInspector inspector = new JdepsInspector(Paths.get("./src/test/resources/slf4j-api-1.8.0-beta2.jar"));
         JdepsInspector.JdepsInspectResult result = inspector.inspect();
 
@@ -19,7 +19,7 @@ public class JdepsInspectorTest {
 
 
     @Test
-    public void testNettyViolatingJar() {
+    void testNettyViolatingJar() {
         JdepsInspector inspector = new JdepsInspector(Paths.get("./src/test/resources/netty-handler-4.1.13.Final.jar"));
         JdepsInspector.JdepsInspectResult result = inspector.inspect();
 
@@ -30,7 +30,7 @@ public class JdepsInspectorTest {
     }
 
     @Test
-    public void testNonReadableJar() {
+    void testNonReadableJar() {
         JdepsInspector inspector = new JdepsInspector(Paths.get("./src/test/resources/non-readable.jar"));
         JdepsInspector.JdepsInspectResult result = inspector.inspect();
 
@@ -38,13 +38,13 @@ public class JdepsInspectorTest {
     }
 
     @Test
-    public void testAkkaViolatingJar() {
+    void testAkkaViolatingJar() {
         JdepsInspector inspector = new JdepsInspector(Paths.get("./src/test/resources/akka-actor_2.11-2.4.9.jar"));
         JdepsInspector.JdepsInspectResult result = inspector.inspect();
 
         assertFalse(result.toolerror);
         assertEquals(1, result.violations.size());
-        assertTrue(result.violations.get(0).contains("sun.misc.Unsafe"));
+        assertLinesMatch(List.of("^sun.misc.Unsafe.+$"), result.violations);
     }
 
 
