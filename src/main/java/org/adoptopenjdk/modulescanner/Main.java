8e7e7b2b25814ef66e7818f2f1a54c8c09695f35
Christian Stein <sormuras@gmail.com>
2018-07-12 22:43:32 +0200

    Create modulescanner.csv while walking the target

--- src/main/java/org/adoptopenjdk/modulescanner/Main.java
+++ src/main/java/org/adoptopenjdk/modulescanner/Main.java
@@ -1,5 +1,12 @@
 package org.adoptopenjdk.modulescanner;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.util.List;
+import org.adoptopenjdk.modulescanner.MavenRepoWalker.MavenArtifact;
+import org.adoptopenjdk.modulescanner.ModuleInspector.ModuleInspectResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,6 +26,8 @@ public class Main {
 
     private static String DEFAULT_DIRECTORY_TO_SCAN = "../gs-maven-mirror";
     private static String CUTOFF_DATE = "20170101000000";
+    private static String DEFAULT_OUTPUT_FILE_NAME = "modulescanner.csv";
+    private static String DEFAULT_OUTPUT_DELIMITER = ", ";
 
     /**
      * Main method - entry point for invoking modulescanner
@@ -28,16 +37,31 @@ public class Main {
     public static void main(String[] args) {
         var directoryToScan = Paths.get(args.length > 0 ? args[0] : DEFAULT_DIRECTORY_TO_SCAN);
         var cutoffDate = args.length > 1 ? args[1] : CUTOFF_DATE;
+        var outputFileName = args.length > 2 ? args[2] : DEFAULT_OUTPUT_FILE_NAME;
 
+        // walk(directoryToScan, cutoffDate, new PrintWriter(System.out));
+
+        try (var fw = new FileWriter(outputFileName); var bw = new BufferedWriter(fw); var out = new PrintWriter(bw)) {
+            walk(directoryToScan, cutoffDate, out);
+        } catch (IOException ioe) {
+            LOGGER.error("Walking directory: " + directoryToScan + " failed", ioe);
+            throw new UncheckedIOException("", ioe);
+        }
+    }
+
+    private static void walk(Path directoryToScan, String cutoffDate, PrintWriter out) {
+        out.println(toHead());
         new MavenRepoWalker(directoryToScan, cutoffDate).getArtifactsToInspect()
                 .forEach(artifact -> {
                     JarFile jarFile = toJarFile(artifact.path);
                     if (jarFile != null) {
-                        ModuleInspector.ModuleInspectResult moduleInspectorResult = new ModuleInspector(jarFile).inspect();
-                        JdepsInspector.JdepsInspectResult jdepsInspectorResult = new JdepsInspector(artifact.path).inspect();
+                        var moduleInspectorResult = new ModuleInspector(jarFile).inspect();
+                        var jdepsInspectorResult = new JdepsInspector(artifact.path).inspect();
                         LOGGER.info(artifact + "\n -> " + moduleInspectorResult + "\n -> " + jdepsInspectorResult);
+                        out.println(toLine(artifact, moduleInspectorResult));
                     }
                 });
+        out.flush();
     }
 
     // Convert a given Path to a Jar file for processing
@@ -50,4 +74,34 @@ public class Main {
         }
     }
 
+    // Create CSV header as string
+    private static String toHead() {
+        var columns = List.of(
+                "groupId",
+                "artifactId",
+                "version",
+                "moduleName",
+                "moduleVersion",
+                "moduleMode",
+                "moduleDependencies");
+        return String.join(DEFAULT_OUTPUT_DELIMITER, columns);
+    }
+
+    // Create single CSV line from artifact and module inspection result
+    private static String toLine(MavenArtifact artifact, ModuleInspectResult mir) {
+        var columns = List.of(
+                valueOrDashIfBlank(artifact.groupId),
+                valueOrDashIfBlank(artifact.artifactId),
+                valueOrDashIfBlank(artifact.version),
+                valueOrDashIfBlank(mir.moduleName),
+                valueOrDashIfBlank(mir.moduleVersion),
+                mir.isAutomaticModule ? "automatic" : mir.isExplicitModule ? "explicit" : "-",
+                valueOrDashIfBlank(String.join(" + ", mir.dependencies)));
+        return String.join(DEFAULT_OUTPUT_DELIMITER, columns);
+    }
+
+    // Convert null or blank values to "-".
+    private static String valueOrDashIfBlank(String value) {
+        return (value == null || value.trim().isEmpty() ) ? "-" : value;
+    }
 }
